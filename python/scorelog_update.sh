dc04ae731c32e2bce31e861c3971181ed344f605
Thomas Yu <thomasyu888@gmail.com>
2017-06-20 13:04:41 -0700

    Use script_dir

--- python/scorelog_update.sh
+++ python/scorelog_update.sh
@@ -1,3 +1,3 @@
 # All logs will go in the same directory as the scoring harness
 script_dir=$(dirname $0)
-cd ~/log && mv score.log score`date +"%Y_%m_%d"`.log && touch score.log
+cd $script_dir/log && mv score.log score`date +"%Y_%m_%d"`.log && touch score.log
